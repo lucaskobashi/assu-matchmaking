05366cd3593a88905f8d4e9faa98fc1de6ec25a9
lucas kobashi <lucas.kobashi@cs.usask.ca>
2022-02-15 14:03:13 -0600

    concluded project ~ ish

--- dataParser.py
+++ dataParser.py
@@ -9,3 +9,129 @@ allPeople = []
 for i in entries:
     allPeople.append(Person(i))
 
+findBoo = True
+
+
+def perfectMatch(source, potential):
+
+    # sexuality
+    sexualities = source.checkSexuality()
+    psexualities = potential.checkSexuality()
+
+    if potential.gender not in sexualities:
+        return 0
+    elif source.gender not in psexualities:
+        return 0
+
+    # # hair
+    # if source.hairPreference != 8:  # source has preference
+    #     if source.hairPreference != potential.hair:
+    #         return 0
+
+    # height
+    # if source.heightPreference != 5:
+    #     if source.heightPreference != potential.height:
+    #         return 0
+
+    # chose school
+    # if source.choseSchool != potential.choseSchool:
+    #     return 0
+
+    # find location
+    # if source.findLive != 0:
+    #
+    #     if source.findLive == 1:  # find on campus
+    #         if potential.live == 0:  # they are off campus
+    #             return 0
+    #
+    #     elif source.findLive == 2:  # find off campus
+    #         if potential.live == 1:  # they are on campus
+    #             return 0
+
+    # profanity
+    # if source.findBadWords == 0 and potential.badWords > 0:
+    #     return 0
+
+    # problem
+    # if source.problem != potential.problem:
+    #     return 0
+
+    # music
+    # if source.music != potential.music:
+    #     return 0
+
+    # age preference
+    if source.agePreference != "doesn't matter":
+
+        if source.agePreference == "older":
+            if potential.age < source.age + 3:
+                return 0
+
+        elif source.agePreference == "same age and older":
+            if potential.age < source.age:
+                return 0
+
+        elif source.agePreference == "same age and younger":
+            if potential.age > source.age:
+                return 0
+
+        elif source.agePreference == "same age only":
+            if potential.age != source.age:
+                return 0
+
+    # about them
+    # if source.aboutThem != "doesn't matter":
+    #     if source.aboutThem != potential.aboutYou:
+    #         return 0
+
+    return 1
+
+
+def likeUBack(source, potential):
+    if source in potential.potentialPeople:
+        return True
+    return False
+
+if findBoo:
+    for person in allPeople:
+        for target in allPeople:
+            if person == target:
+                continue
+            elif perfectMatch(person, target) == 1:
+                person.potentialPeople.append(target)
+
+
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        if not likeUBack(i, j):
+            i.removePotential(j)
+
+for i in allPeople:
+    for j in i.potentialPeople:
+        print(i.name + " is matched with " + j.name)
--- person.py
+++ person.py
@@ -9,24 +9,24 @@ class Person:
         '''
 
         :param line:
-        [0] Timestamp
-        [1] I have read and I accept the terms and conditions above
-        [2] Gender Identity
-        [3] Sexual Orientation
-        [4] Year of study
-        [5] NSID
-        [6] Full name
-        [7] How old are you?
-        [8] Student Number
-        [9] Major
-        [10] What age do you prefer to hang with?
-        [11] Your current hair colour is
-        [12] Standing up straight you're	tall
-        [13] What hair colour do you like on others ?
-        [14] What height do you prefer ?
-        [15] I chose this school because of the
+        [0] Option: 0 = boo, 1 = friend
+        [1] Student number
+        [2] Name
+        [3] Age
+        [4] NSID
+        [5] Major
+        [6] Phone
+        [7] Gender
+        [8] Sexual Orientation
+        [9] Year
+        [10] Age preference
+        [11] Hair colour
+        [12] Hair preference
+        [13] Height
+        [14] Height preference
+        [15] I chose this school because
         [16] I plan to stay in school until
-        [17] Are you a student from
+        [17] Student from
         [18] I live
         [19] I would like to meet someone who lives
         [20] What's your take on bad words? (i.e. profanity)
@@ -34,27 +34,55 @@ class Person:
         [22] The best thing you have going is
         [23] What do you look for first in others?
         [24] I do most of my shopping
-        [25] I am fully vaccinated
-        [26] My monthly cell phone bill is
-        [27] In a crowd you stand out because you're	Body piercing is
+        [25] My monthly cell phone bill is
+        [26] In a crowd you stand out because you're
+        [27] Body piercing is
         [28] Most of my tunes are
         [29] I spend most of my time on my phone using
         [30] A first date should be
         [31] What do you do most when not in school?
         [32] The biggest problem facing today's generation is (besides COVID-19)
         [33] My ideal location is
-        [34] Phone number (only digits)
-        [35] What are you looking for?
         '''
 
         # holds Person objects
         self.potentialPeople = []
-        self.name = None
+
+        self.option = None
         self.studentNumber = None
-        self.nsid = None
-        self.lookingFor = None
-        self.sexuality = None
+        self.name = None
         self.age = None
+        self.nsid = None
+        self.major = None
+        self.year = None
+        self.agePreference = None
+        self.choseSchool = None
+        self.planSchool = None
+        self.studentFrom = None
+        self.live = None
+        self.findLive = None
+        self.badWords = None
+        self.findBadWords = None
+        self.aboutYou = None
+        self.aboutThem = None
+        self.shopping = None
+        self.phoneBill = None
+        self.standOut = None
+        self.music = None
+        self.phoneTime = None
+        self.activities = None
+        self.problem = None
+        self.location = None
+
+        self.gender = None
+        self.orientation = None
+        self.hair = None
+        self.hairPreference = None
+        self.height = None
+        self.heightPreference = None
+        self.bodyPiercing = None
+        self.datePreference = None
+
         self.dataParser(line)
 
     def dataParser(self, line):
@@ -64,17 +92,74 @@ class Person:
         :return:
         '''
         items = line.split(',')
-        self.name = items[6]
-        """
-        self.studentNumber = [8]
-        self.nsid = [5]
-        self.lookingFor = [35]
-        self.sexuality = [
-        
-        """
+
+        self.option = int(items[0])
+        self.studentNumber = int(items[1])
+        self.name = items[2]
+        self.age = int(items[3])
+        self.nsid = items[4]
+        self.major = items[5]
+        self.year = items[9]
+        self.agePreference = items[10]
+        self.choseSchool = int(items[15])
+        self.planSchool = items[16]
+        self.studentFrom = items[17]
+        self.live = int(items[18])
+        self.findLive = int(items[19])
+        self.badWords = int(items[20])
+        self.findBadWords = int(items[21])
+        self.aboutYou = items[22]
+        self.aboutThem = items[23]
+        self.shopping = int(items[24])
+        self.phoneBill = items[25]
+        self.standOut = int(items[26])
+        self.music = items[28]
+        self.phoneTime = int(items[29])
+        self.activities = int(items[31])
+        self.problem = int(items[32])
+        self.location = int(items[33])
+
+        if self.option == 1:
+            self.booPreferences(items)
+
         return 0
 
-    def checkSexuality(self, sexuality):
-        if sexuality == "Straight":
-            return True
-        return False
+    def booPreferences(self, items):
+
+        self.gender = items[7]
+        self.orientation = items[8]
+        self.hair = int(items[11])
+        self.hairPreference = int(items[12])
+        self.height = int(items[13])
+        self.heightPreference = int(items[14])
+        self.bodyPiercing = items[27]
+        self.datePreference = items[30]
+
+        return 0
+
+    def checkSexuality(self):
+
+        possibleGenders = []
+
+        if self.orientation == "Straight":
+            if self.gender == "Man":
+                possibleGenders.append("Woman")
+            elif self.gender == "Woman":
+                possibleGenders.append("Man")
+
+        elif self.orientation == "Gay":
+            possibleGenders.append("Man")
+
+        elif self.orientation == "Lesbian":
+            possibleGenders.append("Woman")
+
+        else:
+            possibleGenders.append("Woman")
+            possibleGenders.append("Man")
+
+        return possibleGenders
+
+    def removePotential(self, person):
+        if person in self.potentialPeople:
+            self.potentialPeople.remove(person)
+        return 0
